bd53efac15c17aa6f8e729cf276f833d55393c70
maxshtun999 <maxshtun99@users.noreply.github.com>
2019-01-31 13:38:39 +0100

    working on adding jinjas and trying to create some small web-site also l hide setting because the secret key is there

--- django1/urls.py
+++ django1/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-
+from django.conf.urls import include, url
+from django.urls import path
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    url(r'^$', include('mainApp.urls')),
 ]
