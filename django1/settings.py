001afeec43dd926d02d413a4727161e0320427cb
Yevhenii <mirandoos3223@gmail.com>
2019-02-14 10:11:44 +0100

    Merge branch 'max' of https://github.com/maxshtun999/Django-the-first into news
    # Conflicts:
    #	contacts/templates/contacts/index.html
    #	db.sqlite3
    #	django1/settings.py
    #	django1/urls.py
    #	mainApp/templates/basic.html
    #	mainApp/templates/includes/some_html.html
    #	mainApp/templates/wrapper.html
    #	news/apps.py
    #	news/migrations/0001_initial.py
    #	news/migrations/0002_news_news_body.py
    #	news/models.py
    #	news/templates/news/main.html
    #	news/urls.py
    #	news/views.py

--- django1/settings.py
+++ django1/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'test_snippets.apps.TestSnippetsConfig',
     'rest_framework',
     'Help.apps.HelpConfig',
     'news.apps.NewsConfig',
@@ -46,6 +47,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
